0d57c2d7a78df49288b2d8e9f86796a1f3bf1b3f
Miguel <Miguel.Angel.G.H.155@gmail.com>
2021-04-19 20:50:48 +0200

    Commit proyecto tarea4

---
+++ src/main/java/PEED4/Main.java
@@ -0,0 +1,52 @@
+package PEED4;
+
+public class Main { 
+
+    public static void main(String[] args) {
+
+        global_monedero (1000);
+
+    }
+
+    public static void global_monedero (double dinero) {
+        Emonedero mimonedero;
+        
+        
+        mimonedero = new Emonedero("Nombre y apellidos del Alumno",500, "dni del alumno");
+        
+        try
+            
+        {
+
+            mimonedero.pagar(200);
+
+        } catch (Exception e)
+
+        {
+
+            System.out.print("No se puede pagar  ");
+
+        }
+
+        try
+
+        {
+
+            System.out.println("Debes recargar monedero");
+
+            mimonedero.recargar(300);
+
+        } catch (Exception e)
+
+        {
+
+            System.out.print("Error al recargar");
+
+        }
+
+        double disponibilidad = mimonedero.efectivo();
+
+        System.out.println("El efectivo del monedero es  "+ disponibilidad );
+    }
+
+}
